c8e829d791ef48cef1e58b1a821d1b3170264aaa
rayan2228 <rayanh.dev@gmail.com>
2023-05-26 16:02:22 +0600

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 import Img from './layouts/Img'
 import SidebarButton from './layouts/SidebarButton';
 import { AiTwotoneHome } from "react-icons/Ai";
+import { RxChatBubble } from "react-icons/rx";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { FaUserFriends } from "react-icons/fa";
+import { BsFilterCircle } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import Flex from "../components/layouts/Flex";
 const Sidebar = () => {
   return (
-    <div className="w-[15%] h-screen  pt-[50px] pl-6  shadow-sm  shadow-shadow capitalize">
-      <Img src="../../public/assets/logo.webp" alt="logo" />
-      <SidebarButton/>
-      <SidebarButton/>
+    <Flex className="flex-col justify-between shadow-sm w-[200px] shadow-shadow  pt-[50px] pl-6 h-screen pb-10 ">
+      <div className="capitalize">
+        <Img src="../../public/assets/logo.webp" alt="logo" />
+        <SidebarButton linkName="home"><AiTwotoneHome className='text-xl' /></SidebarButton>
+        <SidebarButton linkName="chat"><RxChatBubble className='text-xl' /></SidebarButton>
+        <SidebarButton linkName="groups"><HiOutlineUserGroup className='text-xl' /></SidebarButton>
+        <SidebarButton linkName="friends"><FaUserFriends className='text-xl' /></SidebarButton>
+        <SidebarButton linkName="pepole"><BsFilterCircle className='text-xl' /></SidebarButton>
+      </div>
+      <a href="">
+        <Flex className="items-center gap-x-2">
+          <Img src="../../public/assets/user.png" alt="user"/>
+          <div className='mr-4'>
+            <h2>Paula Mora</h2>
+            <h5>Edit Profile</h5>
+          </div>
+            <FiSettings/>
+        </Flex>
+      </a>
+    </Flex>
 
-    </div>
   );
 }
 
