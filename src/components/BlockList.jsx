ec864015051f3ca5d5e68d244bfcd6f7558903dc
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-06-19 11:07:09 +0600

    unblock system done

--- src/components/BlockList.jsx
+++ src/components/BlockList.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { BsSearch } from "react-icons/bs";
 import PeopleLayout from "./layouts/PeopleLayout";
+import { getDatabase, ref, onValue, remove } from "firebase/database";
+import { getAuth } from "firebase/auth";
 const BlockList = () => {
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
+  const [blockList, setBlockList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const db = getDatabase();
+  const blockRef = ref(db, "blocks/");
+  const userRef = ref(db, "users/");
+  useEffect(() => {
+    onValue(blockRef, (snapshot) => {
+      const blockList = [];
+      snapshot.forEach((blocks) => {
+        blockList.push({
+          currentUserId: blocks.val().currentUserId,
+          blockId: blocks.val().blockId,
+          key: blocks.key,
+        });
+      });
+      setBlockList(blockList);
+    });
+    onValue(userRef, (snapshot) => {
+      const users = [];
+      snapshot.forEach((user) => {
+        users.push({ ...user.val(), userId: user.key });
+      });
+      setUsers(users);
+    });
+  }, []);
+  const HandleUnblock = (key) => {
+    remove(ref(db, "blocks/" + key));
+  };
   return (
     <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
       <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
@@ -18,22 +50,32 @@ const BlockList = () => {
         <BsSearch className="absolute top-[53%] left-7 translate-x-[-50%] translate-y-[-50%] text-2xl" />
       </div>
       <div className="h-[40vh]  overflow-y-auto ">
-        <PeopleLayout
-          src="../../../public/assets/friend.png"
-          name="Jenny Wilson"
-          classNameFlex="gap-x-4"
-          classNameHeading="w-[60%]"
-        >
-          <p
-            className="text-lg font-normal text-center text-white capitalize bg-red-500 rounded-md cursor-pointer font-inter w-[30%]"
-            // onClick={() => HandleCancel(reqId.key)}
-          >
-            unblock
-          </p>
-        </PeopleLayout>
+        {blockList.map(
+          (blocks) =>
+            blocks.currentUserId === currentUser.uid &&
+            users.map(
+              (val) =>
+                val.userId === blocks.blockId && (
+                  <PeopleLayout
+                    src={val.profile_picture}
+                    name={val.username}
+                    classNameFlex="gap-x-4"
+                    classNameHeading="w-[55%]"
+                    key={val.userId}
+                  >
+                    <p
+                      className="text-lg font-normal text-center text-white capitalize bg-red-500 rounded-md cursor-pointer font-inter w-[30%]"
+                      onClick={() => HandleUnblock(blocks.key)}
+                    >
+                      unblock
+                    </p>
+                  </PeopleLayout>
+                )
+            )
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default BlockList
+export default BlockList;
