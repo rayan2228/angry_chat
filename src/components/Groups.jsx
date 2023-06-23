5b319e4517d0fbe70b3fc523f8427987d1a4a901
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-06-23 11:37:50 +0600

    dynamic heading

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -4,7 +4,7 @@ import ChatLayout from "./layouts/ChatLayout";
 const Groups = ({ heading }) => {
   return (
     <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
-      <h2 className="text-2xl font-semibold font-inter text-textColor capitalize">
+      <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
         {heading}
       </h2>
       <div className="relative mt-4 mb-5">
