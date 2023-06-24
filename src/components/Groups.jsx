d1bccf10968fb4637785f917c9c891cf4be4c3ce
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-06-24 13:02:46 +0600

    create grp pop and validation

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { BsSearch } from "react-icons/bs";
+import SearchInput from "./layouts/SearchInput";
 import ChatLayout from "./layouts/ChatLayout";
 const Groups = ({ heading }) => {
   return (
     <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
       <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
         {heading}
-        
       </h2>
-      <div className="relative mt-4 mb-5">
-        <input
-          type="search"
-          name=""
-          id=""
-          className="w-full p-2 pl-[53px] rounded-md outline-none placeholder:font-inter placeholder:font-normal placeholder:text-sm placeholder:capitalize placeholder:text-secondaryTextColor border border-[#D3D3D3]"
-          placeholder="search"
-        />
-        <BsSearch className="absolute top-[53%] left-7 translate-x-[-50%] translate-y-[-50%] text-2xl" />
-      </div>
+      <SearchInput  />
       <div className="h-[40vh]  overflow-y-auto ">
         <ChatLayout
           src="../../../public/assets/group.png"
--- src/components/UserSidebar.jsx
+++ src/components/UserSidebar.jsx
@@ -1,148 +1,194 @@
-import React, { useState } from 'react'
-import Flex from './layouts/Flex'
-import SearchInput from './layouts/SearchInput'
-import ChatLayout from './layouts/ChatLayout'
+import React, { useState } from "react";
+import Flex from "./layouts/Flex";
+import SearchInput from "./layouts/SearchInput";
+import ChatLayout from "./layouts/ChatLayout";
 import Input from "../components/layouts/Input";
 const UserSidebar = () => {
-    const [createGroupShow, setCreateGroupShow] = useState(false)
-    return (<>
-        {createGroupShow && (
-            <div className="w-screen h-screen fixed bg-[rgba(50,55,92,0.35)] flex justify-center items-center z-10">
-                <Flex className="w-[500px] bg-primary rounded-lg p-7 shadow-primary_shadow flex-col items-center gap-y-4">
-                    <div className=" w-full text-white ">
-                        <Input inputClass="text-black rounded-md" type="text"
-                            label="Group Name" />
-                        <p className="text-sm font-medium text-red-500 font-inter">
-                            {error ? error : ""}
-                        </p>
-                        <Input inputClass="text-black rounded-md" type="text"
-                            label="Group Tagline" />
-                        <p className="text-sm font-medium text-red-500 font-inter">
-                            {error ? error : ""}
-                        </p>
-                    </div>
-                    <button
-                        className="w-full py-2 text-lg font-semibold text-white capitalize bg-red-500 rounded-lg font-inter "
-                        onClick={() => {
-                            setCreateGroupShow(false), setPassword("");
-                        }}
-                    >
-                        cancel
-                    </button>
-                </Flex>
-            </div>
-        )}
-        <Flex className="h-screen flex-col py-[50px]  w-[430px] pr-4  border-r-2 border-[#D3D3D3]">
-            <Flex className="justify-between mb-6">
-                <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
-                    Group
-                </h2>
-                <button className='text-primary font-inter font-semibold text-lg border-[2px] border-primary py-[10px] px-6 rounded-md' onClick={() => setCreateGroupShow(true)}>Create Group</button>
-            </Flex>
-            <SearchInput />
-            <div className='h-screen  overflow-y-auto'>
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
-                <ChatLayout
-                    src="../../../public/assets/chat.png"
-                    name="Jenny Wilson"
-                    message="Love You....."
-                    time="10:30 PM"
-                />
+  const [createGroupShow, setCreateGroupShow] = useState(false);
+  const [createGroup, setCreateGroup] = useState({
+    groupName: "",
+    groupTag: "",
+  });
+  const [error, setError] = useState("");
+  const handleSetGroup = (e) => {
+    const updateValue = {
+      ...createGroup,
+      [e.target.name]: e.target.value,
+    };
+    setCreateGroup(updateValue);
+  };
+  const handleCreateGroup = () => {
+    if (!createGroup.groupName || !createGroup.groupTag) {
+      setError("all field are required");
+    } else {
+      
+    }
+  };
+  return (
+    <>
+      {createGroupShow && (
+        <div className="w-screen h-screen fixed bg-[rgba(50,55,92,0.35)] flex justify-center items-center z-10">
+          <Flex className="w-[500px] bg-primary rounded-lg p-7 shadow-primary_shadow flex-col items-center gap-y-4">
+            <div className="w-full text-white ">
+              {error && (
+                <p className="p-4 text-sm font-medium capitalize bg-red-500 rounded-md font-inter">
+                  {error}
+                </p>
+              )}
 
+              <Input
+                inputClass="text-black rounded-md"
+                type="text"
+                label="Group Name"
+                value={createGroup.groupName}
+                name="groupName"
+                handle={handleSetGroup}
+              />
+              <Input
+                inputClass="text-black rounded-md"
+                type="text"
+                label="Group Tagline"
+                value={createGroup.groupTag}
+                name="groupTag"
+                handle={handleSetGroup}
+              />
             </div>
+            <button
+              className="w-full py-2 text-lg font-semibold capitalize bg-white rounded-lg font-inter "
+              onClick={handleCreateGroup}
+            >
+              create group
+            </button>
+            <button
+              className="w-full py-2 text-lg font-semibold text-white capitalize bg-red-500 rounded-lg font-inter "
+              onClick={() => {
+                setCreateGroupShow(false),
+                  setCreateGroup({
+                    groupName: "",
+                    groupTag: "",
+                  }),
+                  setError("");
+              }}
+            >
+              cancel
+            </button>
+          </Flex>
+        </div>
+      )}
+      <Flex className="h-screen flex-col py-[50px]  w-[430px] pr-4  border-r-2 border-[#D3D3D3]">
+        <Flex className="justify-between mb-6">
+          <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
+            Group
+          </h2>
+          <button
+            className="text-primary font-inter font-semibold text-lg border-[2px] border-primary py-[10px] px-6 rounded-md"
+            onClick={() => setCreateGroupShow(true)}
+          >
+            Create Group
+          </button>
         </Flex>
+        <SearchInput />
+        <div className="h-screen overflow-y-auto">
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+          <ChatLayout
+            src="../../../public/assets/chat.png"
+            name="Jenny Wilson"
+            message="Love You....."
+            time="10:30 PM"
+          />
+        </div>
+      </Flex>
     </>
+  );
+};
 
-    )
-}
-
-export default UserSidebar
+export default UserSidebar;
