eaa583941a083abbf990c3b9d98e46121828229c
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-07-08 18:12:26 +0600

    group

--- src/components/Groups.jsx
+++ src/components/Groups.jsx
@@ -11,6 +11,9 @@ import {
 import PeopleLayout from "./layouts/PeopleLayout";
 import Option from "./layouts/option";
 import NoData from "./layouts/NoData";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
+import Flex from "./layouts/Flex";
 const Groups = ({ heading }) => {
   const db = getDatabase();
   const currentUser = JSON.parse(localStorage.getItem("userInfo"));
@@ -20,6 +23,8 @@ const Groups = ({ heading }) => {
   const [users, setUsers] = useState([]);
   const [groups, setGroups] = useState([]);
   const [groupRequests, setGroupRequests] = useState([]);
+  const [sendGroupReq, setSendGroupReq] = useState(false);
+  const [show, setShow] = useState(false);
   useEffect(() => {
     onValue(userRef, (snapshot) => {
       const users = [];
@@ -57,43 +62,127 @@ const Groups = ({ heading }) => {
   };
   const handleCancel = (id) => {
     remove(ref(db, "groupRequests/" + id));
+    console.log(id);
   };
   return (
-    <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
-      <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
-        groups
-      </h2>
-      <SearchInput />
-      <div className="h-[40vh]  overflow-y-auto ">
-        {groups.length ? (
-          groups.map(
-            (group) =>
-              group.adminId != currentUser.uid &&
-              !(
-                groupRequests.includes(group.key + currentUser.uid) ||
-                groupRequests.includes(currentUser.uid + group.key)
-              ) && (
-                <PeopleLayout
-                  src={group.groupImg}
-                  name={group.groupName}
-                  classNameFlex="gap-x-4"
-                  classNameHeading="w-[75%]"
-                  key={group.key}
-                >
-                  <p
-                    className="font-inter font-normal text-lg capitalize text-white cursor-pointer w-[24%] bg-primary text-center rounded-md"
-                    onClick={() => handleJoin(group)}
-                  >
-                    join
-                  </p>
-                </PeopleLayout>
+    <>
+      {!sendGroupReq && (
+        <div className="relative w-1/3 p-4 overflow-hidden duration-75 rounded-xl hover:shadow-primary_shadow">
+          {show && (
+            <div className="absolute w-full h-full bg-[#140e0ebf] z-10 top-0 left-0 ">
+              <RxCross2
+                className="absolute text-xl text-white cursor-pointer top-3 right-3"
+                onClick={() => setShow(false)}
+              />
+              <p
+                className="absolute px-6 py-3 text-lg font-normal text-black capitalize bg-white rounded-md cursor-pointer top-10 right-3 font-inter"
+                onClick={() => {
+                  setSendGroupReq(true), setShow(false);
+                }}
+              >
+                View Sended Group Requests
+              </p>
+            </div>
+          )}
+          <Flex className="items-center justify-between">
+            <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
+              groups
+            </h2>
+            <BsThreeDotsVertical
+              className="cursor-pointer"
+              onClick={() => setShow(true)}
+            />
+          </Flex>
+          <SearchInput />
+          <div className="h-[40vh]  overflow-y-auto ">
+            {groups.length ? (
+              groups.map(
+                (group) =>
+                  group.adminId != currentUser.uid &&
+                  !(groupRequests.includes(group.key + currentUser.uid) ||
+                    groupRequests.includes(currentUser.uid + group.key)) && (
+                    <PeopleLayout
+                      src={group.groupImg}
+                      name={group.groupName}
+                      classNameFlex="gap-x-4"
+                      classNameHeading="w-[75%]"
+                      key={group.key}
+                    >
+                      <p
+                        className="font-inter font-normal text-lg capitalize text-white cursor-pointer w-[24%] bg-primary text-center rounded-md"
+                        onClick={() => handleJoin(group)}
+                      >
+                        join
+                      </p>
+                    </PeopleLayout>
+                  )
               )
-          )
-        ) : (
-          <NoData text="no group to show" />
-        )}
-      </div>
-    </div>
+            ) : (
+              <NoData text="no group to show" />
+            )}
+          </div>
+        </div>
+      )}
+      {sendGroupReq && (
+        <div className="relative w-1/3 p-4 overflow-hidden duration-75 rounded-xl hover:shadow-primary_shadow">
+          {show && (
+            <div className="absolute w-full h-full bg-[#140e0ebf] z-10 top-0 left-0 ">
+              <RxCross2
+                className="absolute text-xl text-white cursor-pointer top-3 right-3"
+                onClick={() => setShow(false)}
+              />
+              <p
+                className="absolute px-6 py-3 text-lg font-normal text-black capitalize bg-white rounded-md cursor-pointer top-10 right-3 font-inter"
+                onClick={() => {
+                  setSendGroupReq(false), setShow(false);
+                }}
+              >
+                View Groups
+              </p>
+            </div>
+          )}
+          <Flex className="items-center justify-between">
+            <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
+              group requests
+            </h2>
+            <BsThreeDotsVertical
+              className="cursor-pointer"
+              onClick={() => setShow(true)}
+            />
+          </Flex>
+          <SearchInput />
+          <div className="h-[40vh]  overflow-y-auto ">
+            {groups.length ? (
+              groups.map(
+                (group) =>
+                  group.adminId != currentUser.uid &&
+                  (
+                    groupRequests.includes(group.key + currentUser.uid) ||
+                    groupRequests.includes(currentUser.uid + group.key)
+                  ) && (
+                    <PeopleLayout
+                      src={group.groupImg}
+                      name={group.groupName}
+                      classNameFlex="gap-x-4"
+                      classNameHeading="w-[75%]"
+                      key={group.key}
+                    >
+                      <p
+                        className="font-inter font-normal text-lg capitalize text-white cursor-pointer w-[24%] bg-red-500 text-center rounded-md"
+                        onClick={() => handleCancel(group.key)}
+                      >
+                        cancel
+                      </p>
+                    </PeopleLayout>
+                  )
+              )
+            ) : (
+              <NoData text="no group to show" />
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
