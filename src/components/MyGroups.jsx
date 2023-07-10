1b0d049139a7b815f8e615ebdef85e332510c8a8
rayan <rhrayan2228@gmail.com>
2023-07-10 22:31:57 +0600

    member request show

--- src/components/MyGroups.jsx
+++ src/components/MyGroups.jsx
@@ -18,9 +18,14 @@ const MyGroups = () => {
   const currentUser = JSON.parse(localStorage.getItem("userInfo"));
   // const userRef = ref(db, "users/");
   const groupRef = ref(db, "groups/");
+  const groupRequestRef = ref(db, "groupRequests/");
   // const [users, setUsers] = useState([]);
   const [groups, setGroups] = useState([]);
   const [show, setShow] = useState(false);
+  const [notShow, setNotShow] = useState(true);
+  const [memberRequests, setMemberRequests] = useState([]);
+  const [groupRequestKey, setGroupRequestKey] = useState("");
+
   useEffect(() => {
     // onValue(userRef, (snapshot) => {
     //   const users = [];
@@ -37,23 +42,65 @@ const MyGroups = () => {
       setGroups(groups);
     });
   }, []);
-  const handleShow = ()=>{
-    setShow(true)
-    console.log("asdf");
-  }
+  const handleShow = (key) => {
+    setShow(true);
+    setNotShow(false);
+    setGroupRequestKey(key);
+    onValue(groupRequestRef, (snapshot) => {
+      const memberRequests = [];
+      snapshot.forEach((memberRequest) => {
+          memberRequests.push({
+            ...memberRequest.val(),
+            memberRequestKey: memberRequest.key,
+          });
+      });
+      setMemberRequests(memberRequests);
+    });
+  };
+  console.log(memberRequests);
   return (
     <>
       {show && (
         <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
-          <Flex className="items-center justify-between">
+          <Flex className="items-center justify-between relative">
             <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
               member request
             </h2>
             <RxCross2
-              className="absolute text-xl text-white cursor-pointer top-3 right-3"
-              onClick={() => setShow(false)}
+              className="absolute text-xl text-black cursor-pointer top-2 right-3"
+              onClick={() => {
+                setShow(false), setNotShow(true);
+              }}
             />
           </Flex>
+          <SearchInput />
+          <div className="h-[40vh]  overflow-y-auto ">
+            {memberRequests.length ? (
+              memberRequests.map(
+                (memberRequest) =>
+                  memberRequest.groupKey === groupRequestKey && (
+                    <PeopleLayout
+                      src={memberRequest.requestImg}
+                      name={memberRequest.requestName}
+                      classNameFlex="gap-x-4"
+                      classNameHeading="w-[75%]"
+                      key={memberRequest.memberRequestKey}
+                    >
+                      {/* <Option first="requests" second="delete"  /> */}
+                    </PeopleLayout>
+                  )
+              )
+            ) : (
+              <NoData text="create a new group" />
+            )}
+          </div>
+        </div>
+      )}
+      {notShow && (
+        <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
+          <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
+            my groups
+          </h2>
           <SearchInput />
           <div className="h-[40vh]  overflow-y-auto ">
             {groups.length ? (
@@ -67,7 +114,11 @@ const MyGroups = () => {
                       classNameHeading="w-[75%]"
                       key={group.key}
                     >
-                      {/* <Option first="requests" second="delete"  /> */}
+                      <Option
+                        first="requests"
+                        second="delete"
+                        handleFirst={() => handleShow(group.key)}
+                      />
                     </PeopleLayout>
                   )
               )
@@ -77,32 +128,6 @@ const MyGroups = () => {
           </div>
         </div>
       )}
-      <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
-        <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
-          my groups
-        </h2>
-        <SearchInput />
-        <div className="h-[40vh]  overflow-y-auto ">
-          {groups.length ? (
-            groups.map(
-              (group) =>
-                group.adminId == currentUser.uid && (
-                  <PeopleLayout
-                    src={group.groupImg}
-                    name={group.groupName}
-                    classNameFlex="gap-x-4"
-                    classNameHeading="w-[75%]"
-                    key={group.key}
-                  >
-                    <Option first="requests" second="delete" handleFirst={()=>handleShow()}/>
-                  </PeopleLayout>
-                )
-            )
-          ) : (
-            <NoData text="create a new group" />
-          )}
-        </div>
-      </div>
     </>
   );
 };
