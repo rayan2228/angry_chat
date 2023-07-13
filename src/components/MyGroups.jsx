9a2d52153d1dea0e85a3c6fcd3dc7de45dbad020
rayan <rhrayan2228@gmail.com>
2023-07-13 23:08:03 +0600

    group member show delete

--- src/components/MyGroups.jsx
+++ src/components/MyGroups.jsx
@@ -16,25 +16,28 @@ import Flex from "./layouts/Flex";
 const MyGroups = () => {
   const db = getDatabase();
   const currentUser = JSON.parse(localStorage.getItem("userInfo"));
-  // const userRef = ref(db, "users/");
+  const userRef = ref(db, "users/");
   const groupRef = ref(db, "groups/");
   const groupRequestRef = ref(db, "groupRequests/");
   const groupMemberRef = ref(db, "groupMembers/");
-  // const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
   const [groups, setGroups] = useState([]);
   const [show, setShow] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
   const [notShow, setNotShow] = useState(true);
   const [memberRequests, setMemberRequests] = useState([]);
+  const [members, setMembers] = useState([]);
   const [groupRequestKey, setGroupRequestKey] = useState("");
+  const [groupMemberKey, setGroupMemberKey] = useState("");
 
   useEffect(() => {
-    // onValue(userRef, (snapshot) => {
-    //   const users = [];
-    //   snapshot.forEach((user) => {
-    //     users.push({ ...user.val(), userId: user.key });
-    //   });
-    //   setUsers(users);
-    // });
+    onValue(userRef, (snapshot) => {
+      const users = [];
+      snapshot.forEach((user) => {
+        users.push({ ...user.val(), userId: user.key });
+      });
+      setUsers(users);
+    });
     onValue(groupRequestRef, (snapshot) => {
       const memberRequests = [];
       snapshot.forEach((memberRequest) => {
@@ -53,13 +56,33 @@ const MyGroups = () => {
       setGroups(groups);
     });
   }, []);
+  const handleShowInfo = (key) => {
+    setShowInfo(true);
+    setShow(false);
+    setNotShow(false);
+    setGroupMemberKey(key);
+    onValue(groupMemberRef, (snapshot) => {
+      const members = [];
+      snapshot.forEach((member) => {
+        members.push({
+          ...member.val(),
+          memberKey: member.key,
+        });
+      });
+      setMembers(members);
+    });
+  };
   const handleShow = (key) => {
     setShow(true);
     setNotShow(false);
+    setShowInfo(false);
     setGroupRequestKey(key);
   };
   const HandleCancel = (id) => {
     remove(ref(db, "groupRequests/" + id));
+  };
+  const handleRemoveMember = (id) => {
+    remove(ref(db, "groupMembers/" + id));
   };
   const handleDelete = (key) => {
     memberRequests.map((requests) => {
@@ -76,8 +99,8 @@ const MyGroups = () => {
       groupName: member.groupName,
       groupTag: member.groupTag,
       groupImg: member.groupImg,
-      requestId: member.requestId,
-    }).then(()=>{
+      memberId: member.requestId,
+    }).then(() => {
       remove(ref(db, "groupRequests/" + member.memberRequestKey));
     });
   };
@@ -92,7 +115,7 @@ const MyGroups = () => {
             <RxCross2
               className="absolute text-xl text-black cursor-pointer top-2 right-3"
               onClick={() => {
-                setShow(false), setNotShow(true);
+                setShowInfo(false), setShow(false), setNotShow(true);
               }}
             />
           </Flex>
@@ -133,6 +156,51 @@ const MyGroups = () => {
           </div>
         </div>
       )}
+      {showInfo && (
+        <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
+          <Flex className="relative items-center justify-between">
+            <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
+              group info
+            </h2>
+            <RxCross2
+              className="absolute text-xl text-black cursor-pointer top-2 right-3"
+              onClick={() => {
+                setShowInfo(false), setShow(false), setNotShow(true);
+              }}
+            />
+          </Flex>
+          <SearchInput />
+          <div className="h-[40vh]  overflow-y-auto ">
+            {members.length ? (
+              members.map(
+                (member) =>
+                  member.groupKey === groupMemberKey &&
+                  users.map(
+                    (user) =>
+                      member.memberId === user.userId && (
+                        <PeopleLayout
+                          src={user.profile_picture}
+                          name={user.username}
+                          classNameFlex="gap-x-4"
+                          classNameHeading="w-[75%]"
+                          key={user.userId}
+                        >
+                          <p
+                            className="font-inter font-normal text-lg capitalize text-white cursor-pointer w-[25%] bg-red-500 text-center rounded-md"
+                            onClick={() => handleRemoveMember(member.memberKey)}
+                          >
+                            remove
+                          </p>
+                        </PeopleLayout>
+                      )
+                  )
+              )
+            ) : (
+              <NoData text="no members" />
+            )}
+          </div>
+        </div>
+      )}
       {notShow && (
         <div className="w-1/3 p-4 duration-75 rounded-xl hover:shadow-primary_shadow ">
           <h2 className="text-2xl font-semibold capitalize font-inter text-textColor">
@@ -152,10 +220,12 @@ const MyGroups = () => {
                       key={group.key}
                     >
                       <Option
-                        first="requests"
-                        second="delete"
-                        handleFirst={() => handleShow(group.key)}
-                        handleSecond={() => handleDelete(group.key)}
+                        first="info"
+                        second="requests"
+                        third="delete"
+                        handleFirst={() => handleShowInfo(group.key)}
+                        handleSecond={() => handleShow(group.key)}
+                        handleThird={() => handleDelete(group.key)}
                       />
                     </PeopleLayout>
                   )
