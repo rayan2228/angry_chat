0518ad36d4a3a06270ace847b02032c07aac5a50
rayan <rhrayan2228@gmail.com>
2023-07-18 23:30:31 +0600

    camera access done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Img from "./layouts/Img";
 import Flex from "./layouts/Flex";
 import Option from "./layouts/option";
@@ -9,10 +9,32 @@ import {
   BsMic,
   BsSendFill,
 } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
 import ModalImage from "react-modal-image";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 const Message = () => {
+  const [show, setShow] = useState(false);
+  function handleTakePhoto(dataUri) {
+    // Do stuff with the photo...
+    console.log("takePhoto");
+  }
   return (
     <>
+      {show && (
+        <div className="absolute z-10 flex items-center justify-center w-screen h-screen bg-[rgba(50,55,92,0.35)]">
+          <div className="w-[80vw] h-screen">
+            <Camera
+              onTakePhoto={(dataUri) => {
+                handleTakePhoto(dataUri);
+              }}
+              isImageMirror={true}
+              isFullscreen={true}
+            />
+          </div>
+          <RxCross2 className="absolute text-3xl text-white bg-red-500 cursor-pointer top-2 right-4" onClick={()=>setShow(false)}/>
+        </div>
+      )}
       <Flex className="flex-col grow">
         <Flex className="px-4 pt-[50px]  gap-x-4 items-center shadow-primary_shadow pb-4">
           <Img src="../../public/assets/default.png" className="w-[14%]" />
@@ -82,7 +104,10 @@ const Message = () => {
             <Flex className="absolute top-[37%] gap-x-3 right-4 ">
               <BsEmojiSmile className="text-xl cursor-pointer" />
               <BsCardImage className="text-xl cursor-pointer" />
-              <BsCamera className="text-xl cursor-pointer" />
+              <BsCamera
+                className="text-xl cursor-pointer"
+                onClick={() => setShow(true)}
+              />
               <BsMic className="text-xl cursor-pointer" />
               <BsSendFill className="text-xl cursor-pointer text-[#32375C]" />
             </Flex>
