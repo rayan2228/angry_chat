d21488475fc0c60e797238583a34cc8caf786d53
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2023-07-27 17:44:19 +0600

    single plain message done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -32,6 +32,7 @@ const Message = ({ status }) => {
   const [show, setShow] = useState(false);
   const [messageBtnShow, setMessageBtnShow] = useState(false);
   const [message, setMessage] = useState("");
+  const [userMessage, setUserMessage] = useState([]);
   function handleTakePhoto(dataUri) {
     // Do stuff with the photo...
     console.log("takePhoto");
@@ -42,7 +43,18 @@ const Message = ({ status }) => {
     } else {
       setMessageBtnShow(false);
     }
+    onValue(messageRef, (snapshot) => {
+      const userMessage = [];
+      snapshot.forEach((messages) => {
+        userMessage.push({
+          ...messages.val(),
+          userMessageId: messages.key,
+        });
+      });
+      setUserMessage(userMessage);
+    });
   }, [message]);
+  // console.log(userMessage);
 
   const handleMessage = (e) => {
     setMessage(e.target.value);
@@ -102,21 +114,35 @@ const Message = ({ status }) => {
         <Flex className="flex-col h-screen p-6 overflow-y-auto">
           <div className="h-screen overflow-y-auto">
             {/* message receive */}
-            <div className="mt-4 text-left">
-              <div className="inline-block text-lg capitalize font-inter text-[#222222] bg-[#E9E9E9] rounded-md px-6 font-normal py-1">
-                hello
-              </div>
-              <h6 className="mt-2 text-xs font-inter text-slate-600">12:00</h6>
-            </div>
+            {userMessage.map((message) =>
+              message.whoReceived === currentUser.uid &&
+              message.whoSend === activeMessage.userId ? (
+                <div className="mt-4 text-left">
+                  <div className="inline-block text-lg capitalize font-inter text-[#222222] bg-[#E9E9E9] rounded-md px-6 font-normal py-1">
+                    {message.message}
+                  </div>
+                  <h6 className="mt-2 text-xs font-inter text-slate-600">
+                    12:00
+                  </h6>
+                </div>
+              ) : (
+                message.whoReceived === activeMessage.userId &&
+                message.whoSend === currentUser.uid && (
+                  <div className="mt-4 text-right">
+                    <div className=" inline-block text-lg  font-inter text-[#ffffff] bg-[#5B5F7D] rounded-md px-6 font-normal py-1 text-left">
+                      {message.message}
+                    </div>
+                    <h6 className="mt-2 text-xs font-inter text-slate-600">
+                      12:00
+                    </h6>
+                  </div>
+                )
+              )
+            )}
             {/* message send */}
-            <div className="mt-4 text-right">
-              <div className=" inline-block text-lg capitalize font-inter text-[#ffffff] bg-[#5B5F7D] rounded-md px-6 font-normal py-1 text-left">
-                hello
-              </div>
-              <h6 className="mt-2 text-xs font-inter text-slate-600">12:00</h6>
-            </div>
+
             {/* message receive image */}
-            <div className="mt-4 text-left">
+            {/* <div className="mt-4 text-left">
               <div className="inline-block  bg-[#E9E9E9] rounded-md p-2 w-[200px]">
                 <ModalImage
                   small={
@@ -129,16 +155,16 @@ const Message = ({ status }) => {
                 />
               </div>
               <h6 className="mt-2 text-xs font-inter text-slate-600">12:00</h6>
-            </div>
+            </div> */}
             {/* message send */}
-            <div className="mt-4 text-right">
+            {/* <div className="mt-4 text-right">
               <div className=" inline-block text-lg capitalize font-inter text-[#ffffff] bg-[#5B5F7D] rounded-md px-6 font-normal py-1 text-left">
                 hello
               </div>
               <h6 className="mt-2 text-xs font-inter text-slate-600">12:00</h6>
-            </div>
+            </div> */}
             {/* message send img */}
-            <div className="mt-4 text-right">
+            {/* <div className="mt-4 text-right">
               <div className=" inline-block bg-[#5B5F7D] rounded-md p-2 w-[200px]">
                 <ModalImage
                   small={
@@ -151,7 +177,7 @@ const Message = ({ status }) => {
                 />
               </div>
               <h6 className="mt-2 text-xs font-inter text-slate-600">12:00</h6>
-            </div>
+            </div> */}
           </div>
           <div className="relative bg-[#F4F4F4]">
             <textarea
